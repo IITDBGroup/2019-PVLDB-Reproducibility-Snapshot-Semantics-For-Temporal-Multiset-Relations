d327907b7811445355f18d4fdd11674f591f85a7
Boris Glavic <lordpretzel@gmail.com>
2020-01-03 08:07:18 -0600

    build files

---
+++ docker/build-data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build . -f Dockerfile-reproducibility -t iitdbgroup/2019-pvldb-snapshot-temporal-reproducibility-data
--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker build . -f Dockerfile -t iitdbgroup/2019-pvldb-snapshot-temporal-reproducibility-data
 docker build . -f Dockerfile -t iitdbgroup/2019-pvldb-snapshot-temporal-reproducibility
