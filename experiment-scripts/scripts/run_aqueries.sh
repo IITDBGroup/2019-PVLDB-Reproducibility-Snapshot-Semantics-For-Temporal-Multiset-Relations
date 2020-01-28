23dcbdd58cbc73f4eb9cd6df18a9aec0dc1ebfe4
Xing Niu <xniu7@hawk.iit.edu>
2020-01-28 15:11:47 -0600

    up scripts

--- experiment-scripts/scripts/run_aqueries.sh
+++ experiment-scripts/scripts/run_aqueries.sh
@@ -20,26 +20,46 @@ do
          fi;            
 done;
 
-for i in 5 6 7 8 12 14 19 1
+for i in 6 12 19
 do
          if [ ! -f "$DIR/result/aq$i.txt" ];
          then
             echo -e "running q$i ..."
-            $DIR/scripts/time_aquery.sh time_tpch_1gb $DIR/queries/pg_nat/q$i.sql 2 1 > $DIR/result/aq_temp.txt
+            $DIR/scripts/time_aquery.sh time_tpch_1gb $DIR/queries/pg_nat/q$i.sql 100 5 > $DIR/result/aq_temp.txt
+            mv $DIR/result/aq_temp.txt $DIR/result/aq$i.txt
+         fi;            
+done;
+
+for i in 5 7 8 14 1
+do
+         if [ ! -f "$DIR/result/aq$i.txt" ];
+         then
+            echo -e "running q$i ..."
+            $DIR/scripts/time_aquery.sh time_tpch_1gb $DIR/queries/pg_nat/q$i.sql 10 2 > $DIR/result/aq_temp.txt
             mv $DIR/result/aq_temp.txt $DIR/result/aq$i.txt
          fi;            
 done;
 
 
-for i in join1 join3 join4 agg2 agg3 diff1 diff2 agg1 join2 agg_join
+for i in join1 join3 join4 agg2 agg3 diff1 diff2
 do
          if [ ! -f "$DIR/result/a$i.txt" ];
          then
             echo -e "running $i ..."
-            $DIR/scripts/time_aquery.sh employees $DIR/queries/pg_nat/"$i".sql 2 1 > $DIR/result/aq_temp.txt
+            $DIR/scripts/time_aquery.sh employees $DIR/queries/pg_nat/"$i".sql 100 10 > $DIR/result/aq_temp.txt
             mv $DIR/result/aq_temp.txt $DIR/result/a$i.txt
          fi;         
 done;
 
+
+for i in join2 agg1 agg_join
+do
+         if [ ! -f "$DIR/result/a$i.txt" ];
+         then
+            echo -e "running $i ..."
+            $DIR/scripts/time_aquery.sh employees $DIR/queries/pg_nat/"$i".sql 10 2 > $DIR/result/aq_temp.txt
+            mv $DIR/result/aq_temp.txt $DIR/result/a$i.txt
+         fi;         
+done;
 
 
