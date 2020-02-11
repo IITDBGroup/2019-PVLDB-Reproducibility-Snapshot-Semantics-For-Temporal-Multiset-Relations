245e4e78c66360b16ce5496ad3f27c902dd85979
Xing Niu <xniu7@hawk.iit.edu>
2020-02-10 23:30:52 -0600

    fix aq19.sql

--- experiment-scripts/queries/pg_nat/q19.sql
+++ experiment-scripts/queries/pg_nat/q19.sql
@@ -1,13 +1,15 @@
-WITH _temp_view_0 AS (
-SELECT sum(l_extendedprice* (1 - l_discount)) AS "revenue", ts AS "t_b", te AS "t_e"
-FROM (
+WITH temp_view AS (
 SELECT * 
 FROM 
     (time_lineitem a
     PERIOD JOIN WITH (ACTIVE_TIME_BEGIN, ACTIVE_TIME_END, AVAILABLE_TIME_BEGIN, AVAILABLE_TIME_END) AS (ts,te) 
     time_part b
     ON (p_partkey = l_partkey)) x
-WHERE (p_Brand = 'Brand#22' AND (p_container = 'SM CASE' OR p_container = 'SM BOX' OR p_container = 'SM PACK' OR p_container = 'SM PKG') AND l_quantity >= 2 AND l_quantity <= 2 + 10 AND p_size >= 1 AND p_size <= 5 AND (l_shipmode ='AIR' OR l_shipmode = 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') OR (p_partkey = l_partkey AND p_Brand = 'Brand#32' AND (p_container = 'MED BAG' OR p_container = 'MED BOX' OR p_container = 'MED PKG' OR p_container = 'MED PACK') AND l_quantity >= 19 AND l_quantity <= 19 + 10 AND p_size >= 1 AND p_size <= 10 AND (l_shipmode ='AIR' OR l_shipmode = 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') OR (p_partkey = l_partkey AND p_Brand = 'Brand#43' AND (p_container = 'LG CASE' OR p_container = 'LG BOX' OR p_container = 'LG PACK' OR p_container = 'LG PKG') AND l_quantity >= 21 AND l_quantity <= 21 + 10 AND p_size >= 1 AND p_size <= 15 AND (l_shipmode ='AIR' OR l_shipmode = 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON')) y
+),
+_temp_view_0 AS (
+SELECT sum(l_extendedprice* (1 - l_discount)) AS "revenue", ts AS "t_b", te AS "t_e"
+FROM temp_view
+WHERE (p_Brand = 'Brand#22' AND (p_container = 'SM CASE' OR p_container = 'SM BOX' OR p_container = 'SM PACK' OR p_container = 'SM PKG') AND l_quantity >= 2 AND l_quantity <= 2 + 10 AND p_size >= 1 AND p_size <= 5 AND (l_shipmode ='AIR' OR l_shipmode = 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') OR (p_partkey = l_partkey AND p_Brand = 'Brand#32' AND (p_container = 'MED BAG' OR p_container = 'MED BOX' OR p_container = 'MED PKG' OR p_container = 'MED PACK') AND l_quantity >= 19 AND l_quantity <= 19 + 10 AND p_size >= 1 AND p_size <= 10 AND (l_shipmode ='AIR' OR l_shipmode = 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') OR (p_partkey = l_partkey AND p_Brand = 'Brand#43' AND (p_container = 'LG CASE' OR p_container = 'LG BOX' OR p_container = 'LG PACK' OR p_container = 'LG PKG') AND l_quantity >= 21 AND l_quantity <= 21 + 10 AND p_size >= 1 AND p_size <= 15 AND (l_shipmode ='AIR' OR l_shipmode = 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON')
 GROUP BY PERIOD WITH (ts,te)
 )
 SELECT F0."revenue" AS "revenue", F0."t_b" AS "t_b", F0."ts" AS "t_e"
